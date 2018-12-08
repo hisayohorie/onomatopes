ccc23e2fcd225134f2c7db92c00f1bd178be8a79
hisayohorie <hisayohorie@gmail.com>
2018-12-07 20:44:43 -0500

    Running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205184817) do
+ActiveRecord::Schema.define(version: 20181208014250) do
+
+  create_table "examples", force: :cascade do |t|
+    t.string "sentence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jp_sentence"
+  end
 
   create_table "words", force: :cascade do |t|
     t.string "onomatope"
