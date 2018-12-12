5ae501ec6a837b100a924575257380b93feee440
hisayohorie <hisayohorie@gmail.com>
2018-12-11 23:15:59 -0500

    Running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181208014250) do
+ActiveRecord::Schema.define(version: 20181212034845) do
 
   create_table "examples", force: :cascade do |t|
     t.string "sentence"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jp_sentence"
+    t.integer "word_id"
   end
 
   create_table "words", force: :cascade do |t|
